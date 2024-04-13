784bf6303fe49ab5060a2c407c637e2c23ff8a22
Chris Chen <chrischen3121@gmail.com>
2024-04-13 22:31:35 +0800

    updated :lang configuration

--- modules/cc-langs/elisp/config.el
+++ modules/cc-langs/elisp/config.el
@@ -1,6 +1,7 @@
 ;; no-byte-compile: t
 ;;; cc-langs/elisp/config.el -*- lexical-binding: t; -*-
 
-;; (map! :map emacs-lisp-mode-map
-;;       "C-c C-f" nil
-;;       :desc "Byte compile" "C-c l c" #'byte-compile-file)
+(map! :after elisp-mode
+      :map emacs-lisp-mode-map
+      :desc "Lookup demos" "C-c k e" #'elisp-demos-find-demo
+      :desc "Byte compile" "C-c c c"#'byte-compile-file)
