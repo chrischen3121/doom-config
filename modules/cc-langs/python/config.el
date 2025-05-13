2e46f811145fb71b89c7ae0b54e2200e46a5a09b
Chris Chen <chrischen@ignity.xyz>
2025-05-13 23:31:27 +0800

    feat(python): enable mypy and dmypy for pylsp

--- modules/cc-langs/python/config.el
+++ modules/cc-langs/python/config.el
@@ -66,12 +66,13 @@
            lsp-pylsp-plugins-mccabe-threshold 10
            lsp-pylsp-plugins-ruff-enabled t
            lsp-pylsp-plugins-ruff-format t
+           lsp-pylsp-plugins-mypy-enabled t
+           lsp-pylsp-plugins-mypy-dmypy t
            lsp-pylsp-plugins-pydocstyle-enabled nil
            lsp-pylsp-plugins-pycodestyle-enabled nil
            lsp-pylsp-plugins-flake8-enabled nil
            lsp-pylsp-plugins-black-enabled nil
            lsp-pylsp-plugins-isort-enabled nil
-           lsp-pylsp-plugins-mypy-enabled nil
            lsp-pylsp-plugins-pylint-enabled nil
            lsp-pylsp-plugins-pyflakes-enabled nil
            lsp-pylsp-plugins-autopep8-enabled nil
