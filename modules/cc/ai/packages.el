27f827077e82622ebeabd2f93b623e0e7b294542
Chris Chen <chrischen3121@gmail.com>
2025-06-09 20:19:59 +0800

    feat(ai): pin magit to version 4.3.6

--- modules/cc/ai/packages.el
+++ modules/cc/ai/packages.el
@@ -3,6 +3,10 @@
 
 (package! transient
   :pin "13daf473d5080b91bc37f40e0f3b566738707914") ; 0.9.1
+
+(package! magit
+  :pin "fd1882b8c981c5e859522bde1dd9c88af9485709") ; 4.3.6
+
 (package! aider
   :recipe (:host github :repo "tninja/aider.el" ))
 
