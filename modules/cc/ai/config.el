e30074067554a5f60db6c9b4431d37710e8eea94
Chris Chen <chrischen3121@gmail.com>
2025-07-10 11:04:41 +0800

    don't need mcp tools at the point

--- modules/cc/ai/config.el
+++ modules/cc/ai/config.el
@@ -31,8 +31,9 @@
         :desc "Add region/buffer" "a" #'gptel-add
         :desc "Add file" "f" #'gptel-add-file
         :desc "Rewrite region" "r" #'gptel-rewrite
-        :desc "Tools enable" "t" #'cc/gptel-enable-all-mcp-tools
-        :desc "Tools disable" "T" #'cc/gptel-disable-all-mcp-tools)
+        ;; :desc "Tools enable" "t" #'cc/gptel-enable-all-mcp-tools
+        ;; :desc "Tools disable" "T" #'cc/gptel-disable-all-mcp-tools
+        )
   :config
   (setq! gptel-default-mode 'org-mode
          gptel-log-level 'info
@@ -52,21 +53,21 @@
 
 ;; mcp servers
 ;; use `mcp-make-text-tool` to create a gptel tool
-(use-package! mcp-hub
-  :commands (mcp-hub
-             mcp-hub-start-all-server
-             mcp-hub-close-all-server)
-  :init
-  (setq mcp-hub-servers
-        ;; support multiple directories
-        `(("filesystem" .
-           (:command "npx" :args ("-y" "@modelcontextprotocol/server-filesystem" ,cc/mcp-fs-directory)))
-          ("fetch" . (:command "uvx" :args ("mcp-server-fetch")))
-          ;; ("git" . (:command "uvx" :args ("mcp-server-git" "--git-dir" ,cc/mcp-git-directory)))
-          ))
-  (map! :desc "mcp hub" "C-c a m" #'mcp-hub)
-  (when cc/use-mcp-p
-    (after! gptel
-      (cc/gptel-mcp-register-tools))
-    (add-hook 'after-init-hook #'mcp-hub-start-all-server)
-    (add-hook 'gptel-mode-hook #'cc/gptel-enable-all-mcp-tools)))
+;; (use-package! mcp-hub
+;;   :commands (mcp-hub
+;;              mcp-hub-start-all-server
+;;              mcp-hub-close-all-server)
+;;   :init
+;;   (setq mcp-hub-servers
+;;         ;; support multiple directories
+;;         `(("filesystem" .
+;;            (:command "npx" :args ("-y" "@modelcontextprotocol/server-filesystem" ,cc/mcp-fs-directory)))
+;;           ("fetch" . (:command "uvx" :args ("mcp-server-fetch")))
+;;           ;; ("git" . (:command "uvx" :args ("mcp-server-git" "--git-dir" ,cc/mcp-git-directory)))
+;;           ))
+;;   (map! :desc "mcp hub" "C-c a m" #'mcp-hub)
+;;   (when cc/use-mcp-p
+;;     (after! gptel
+;;       (cc/gptel-mcp-register-tools))
+;;     (add-hook 'after-init-hook #'mcp-hub-start-all-server)
+;;     (add-hook 'gptel-mode-hook #'cc/gptel-enable-all-mcp-tools)))
